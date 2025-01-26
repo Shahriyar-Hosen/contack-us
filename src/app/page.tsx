e849014cf113b5a106058937016d861d0537685d
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2025-01-26 11:49:48 +0600

    Add contact form image and update layout on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const HomePage = () => {
   return (
     <div className="flex justify-start items-center w-full flex-col h-svh bg-background text-foreground">
@@ -12,10 +14,31 @@ const HomePage = () => {
           Contact the Hablu programmer team!
         </p>
       </div>
-      <div className="w-full max-w-[1092px] mx-auto bg-[#FFFFFF0A] border-2 rounded-[20px] border-[#0A0D170D] h-[576px] relative">
+      <div className="w-full max-w-[1092px] mx-auto bg-[#FFFFFF0A] border-2 rounded-[20px] border-[#0A0D170D] relative">
         <div className="left-0 top-0 absolute size-[152px] rounded-full blur-[200px] bg-primary" />
         <div className="right-0 top-0 absolute size-[152px] rounded-full blur-[200px] bg-secondary" />
         <div className="left-0 bottom-0 absolute size-[152px] rounded-full blur-[200px] bg-secondary" />
+
+        <div className="w-full h-full p-5 flex justify-between items-center">
+          <div>{/* Form */}</div>
+          <div className="w-full max-w-[516px] relative">
+            <Image
+              alt="Hablu Contact Form Image"
+              src="/contact-form.png"
+              width={516}
+              height={536}
+              className="w-full h-auto rounded-xl"
+            />
+
+            <div className="space-y-1.5 px-7 absolute bottom-10 w-full">
+              <p className="opacity-70">
+                For any problems or assistance, please contact the Hablu
+                programmer team. We are always ready to help! 😊
+              </p>
+              <p className="font-medium opacity-80">Hablu Programmer</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
