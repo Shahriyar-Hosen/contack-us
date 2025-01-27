b62dc97600267a2aa1cb644c0b1ec630d7212ee3
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2025-01-27 12:37:43 +0600

    Implement ContactForm component with state management and form submission handling

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,18 +1,65 @@
+"use client";
+
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Button } from "./button";
 import { Input, Textarea } from "./input";
 
+interface ContactForm {
+  fistName?: string;
+  lastName?: string;
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}
+const defaultValue = { name: "", email: "", phone: "", message: "" };
 export const ContactForm = () => {
-  return (
-    <form className="space-y-3.5">
-      <div className="flex justify-between items-center gap-3.5">
-        <Input placeholder="First Name" />
-        <Input placeholder="Last Name" />
-      </div>
-      <Input placeholder="Email" type="email" />
-      <Input placeholder="Phone Number" />
-      <Textarea placeholder="Message!" />
+  const [data, setData] = useState<ContactForm>(defaultValue);
 
-      <Button>
+  type InputEvent = ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
+  const handleChange = (e: InputEvent) => {
+    const { name, value } = e.target || {};
+    setData((prv) => ({
+      ...prv,
+      [name]: value,
+      name:
+        name === "fistName" || name === "lastName"
+          ? `${name === "fistName" ? value.trim() : data.fistName} ${
+              name === "lastName" ? value.trim() : data.lastName
+            }`
+          : prv.name,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("🚀 ~ ContactForm ~ data:", data);
+  };
+
+  return (
+    <form className="space-y-3.5" onSubmit={handleSubmit}>
+      <div className="flex justify-between items-center gap-3.5">
+        <Input
+          placeholder="First Name"
+          name="fistName"
+          onChange={handleChange}
+        />
+        <Input
+          placeholder="Last Name"
+          name="lastName"
+          onChange={handleChange}
+        />
+      </div>
+      <Input
+        placeholder="Email"
+        name="email"
+        type="email"
+        onChange={handleChange}
+      />
+      <Input placeholder="Phone Number" name="phone" onChange={handleChange} />
+      <Textarea placeholder="Message!" name="message" onChange={handleChange} />
+
+      <Button type="submit">
         Send{" "}
         <span className="rotate-45 block text-2xl leading-none tracking-normal">
           🚀
